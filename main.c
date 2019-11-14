943a0dde96bb4eeb34077e343492df9274cc4ca0
qtgeo1248 <gzhou00@stuy.edu>
2019-11-14 10:06:12 -0500

    fixed up permissions

--- main.c
+++ main.c
@@ -10,9 +10,9 @@ int main() {
     printf("File size: %lld\n", info.st_size);
     printf("perms: %o\n", info.st_mode);
     time_t time = info.st_atime;
-    printf("Time Last Accessed: %s\n\n", ctime(&time));
+    printf("Time Last Accessed: %s\n", ctime(&time));
 
-    printf("Now doing things in Readable Form\n");
+    printf("Now doing things in Readable Form:\n");
     char string[100];
     float size2;
     int size = info.st_size;
@@ -24,28 +24,24 @@ int main() {
     }
     printf("File Size: %s\n", string);
 
-    printf("File permissionss: -");
+    printf("File permissions: -");
     char perm[100];
-    sprintf(perm, "%o", info.st_mode & 511);
+    sprintf(perm, "%o", info.st_mode % 01000);
     int i = 0;
     for (; i < 3; i++) {
-        if (perm[i] == '0') {
-            printf("---");
-        } else if (perm[i] == '1') {
-            printf("--x");
-        } else if (perm[i] == '2') {
-            printf("-w-");
-        } else if (perm[i] == '3') {
-            printf("-wx");
-        } else if (perm[i] == '4') {
-            printf("r--");
-        } else if (perm[i] == '5') {
-            printf("r-x");
-        } else if (perm[i] == '6') {
-            printf("rw-");
-        } else {
-            printf("rwx");
+        int current = perm[i] - '0';
+        char out[3];
+        char results[3] = "rwx";
+        int j = 2;
+        for(; j >= 0; j--) {
+            if (current % 2 == 0) {
+                out[j] = '-';
+            } else {
+                out[j] = results[j];
+            }
+            current /= 2;
         }
+        printf("%s", out);
     }
     printf("\n");
     return 0;
